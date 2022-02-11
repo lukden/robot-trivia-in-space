8688ef8a35d55f317be2006a857989e8b2c8c3ca
lukden <dean.l@northeastern.edu>
2022-02-11 15:36:03 -0500

    Build out html and basic css framework

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*-----------Constants----------*/
+
+import { quizCategories } from "../js-arrays/app.js"
+
+/*-------------Variables-----------*/
+
+
+
+/*-------------Cached Element References----------*/
+
+const aBtn = document.querySelector(#a-button)
+const bBtn = document.querySelector(#b-button)
+const cBtn = document.querySelector(#c-button)
+const dBtn = document.querySelector(#d-button)
+
+const submitBtn = document.querySelector(#submit)
+
+/*---------------Event Listeners---------*/
+
+aBtn.addEventListener("click", logAnswer)
