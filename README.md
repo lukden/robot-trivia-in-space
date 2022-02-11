a7f50f7f6b8769b03e4e2ccf868341dcbecd9ae3
lukden <dean.l@northeastern.edu>
2022-02-11 13:20:19 -0500

    Create quiz template with object array and add science category Q&As

---
+++ README.md
@@ -0,0 +1,1 @@
+# robot-trivia-in-space
---
+++ js-arrays/app.js
@@ -0,0 +1,206 @@
+const quizCategories = {
+  scienceQuestions = [
+    {
+    question = "Which planet is the 3rd closest to the sun?"
+    },
+    answers = {
+      a: "Mars",
+      b: "Uranus",
+      c: "Saturn",
+      d: "Earth"
+    },
+    correctAnswer = {
+      d: "Jupiter"
+    },
+    {
+    question = "Scientists taught rats how to ______. Which option does NOT acurately finsh the sentence?"
+    },
+    answers = {
+      a: "Play Doom II",
+      b: "Drive tiny cars",
+      c: "Cook food",
+      d: "Find landmines"
+    },
+    correctAnswer = {
+      c: "Cook food"
+    },
+    {
+    question = "What is the country of origin of the first living creature sent into space?"
+    },
+    answers = {
+      a: "China",
+      b: "United States of America",
+      c: "Soviet Union",
+      d: "United Kingdom"
+      },
+    correctAnswer = {
+      c: "Soviet Union"
+    },
+    {
+    question = "Dark matter matter makes up this percentage of all matter in the universe?"
+    },
+    answers = {
+      a: "15%",
+      b: "27%",
+      c: "40%",
+      d: "53%"
+    },
+    correctAnswer = {
+      b: "27%"
+    } 
+  ],
+
+  imageQuestions = [
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      c: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+      },
+    correctAnswer = {
+      b: ""
+    },
+  ],
+
+  imageQuestions = [
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      c: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+      },
+    correctAnswer = {
+      b: ""
+    },
+  ],
+
+  imageQuestions = [
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      c: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    correctAnswer = {
+      b: ""
+    },
+    {
+    question = ""
+    },
+    answers = {
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+      },
+    correctAnswer = {
+      b: ""
+    },
+  ],
+
+}
